63f9ada354253a50908f8f85c44b125b9ae6f399
hcc960923 <101438085+hcc960923@users.noreply.github.com>
2022-05-20 17:03:55 +0800

    xxx

--- mini-react/src/index.js
+++ mini-react/src/index.js
@@ -19,22 +19,97 @@ function createTextElement(text) {
     }
   };
 }
-  
-function render(element, container) {
-  const dom =
-    element.type === "TEXT_ELEMENT"
+
+function createDom(fiber) {
+  const dom = 
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
+
   const isProperty = key => key !== "children";
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach(name => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
-  element.props.children.forEach(child => render(child, dom));
-  container.appendChild(dom);
-}
   
+  return dom;
+}
+
+function render(element, container) {
+  // TODO this next unit of work
+
+  // ! 引入fiber架构的原因，递归太耗时，如果是一个big tree
+  // const dom = createDom(element)
+  // element.props.children.forEach(child => render(child, dom));
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element]
+    },
+  }
+
+
+  // container.appendChild(dom);
+  nextUnitOfWork = wipRoot;
+}
+
+let nextUnitOfWork = null;
+let wipRoot = null;
+
+function workLoop(deadline) {
+  nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+}
+
+window.requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  // TODO 1 create a new node and append it to the DOM
+  
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+  // TODO 2 create the fibers for the element’s children
+
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null
+    }
+  }
+  if (index === 0) {
+    fiber.child = newFiber; // first element
+  } else {
+    prevSibling.sibling = newFiber; // !first element
+  }
+
+  prevSibling = newFiber; // !first element as ths prevSibling
+  index++;
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+  // TODO 3 select the next unit of work
+}
+
 const Didact = {
   createElement,
   render
