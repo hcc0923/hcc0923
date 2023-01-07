fdfcd8d619a3a4ad4d52684b196afcb09d81c8e5
hcc <healthyhcc@gmail.com>
2023-01-07 16:47:47 +0800

    delete

--- userInfo.tsx
+++ userInfo.tsx
@@ -6,27 +6,3 @@ export interface RoleInfoProps {
     auth: string,
     role: number
 }
-export interface UserInfoProps1 {
-    name: string,
-    age: number
-}
-export interface UserInfoProps2 {
-    name: string,
-    age: number
-}
-export interface UserInfoProps3 {
-    name: string,
-    age: number
-}
-export interface UserInfoProps4 {
-    name: string,
-    age: number
-}
-export interface UserInfoProps5 {
-    name: string,
-    age: number
-}
-export interface UserInfoProps6 {
-    name: string,
-    age: number
-}
