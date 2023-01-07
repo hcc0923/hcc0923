2c6b9255b152cba39bac8e167d265b2c46174f3a
hcc <healthyhcc@gmail.com>
2023-01-07 15:57:26 +0800

    add: add test user info props

--- userInfo.tsx
+++ userInfo.tsx
@@ -1,9 +1,24 @@
-export type StudentProps  = {
-    name: string;
-    gender: boolean;
-    age: number;
-}
 export interface UserInfoProps {
+    name: string,
+    age: number
+}
+export interface UserInfoProps1 {
+    name: string,
+    age: number
+}
+export interface UserInfoProps2 {
+    name: string,
+    age: number
+}
+export interface UserInfoProps3 {
+    name: string,
+    age: number
+}
+export interface UserInfoProps4 {
+    name: string,
+    age: number
+}
+export interface UserInfoProps5 {
     name: string,
     age: number
 }
