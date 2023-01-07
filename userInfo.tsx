57c70317919b301595c7d04003a23709d3ff8123
hcc <healthyhcc@gmail.com>
2023-01-07 15:28:18 +0800

    fix: student props

--- userInfo.tsx
+++ userInfo.tsx
@@ -1,15 +1,9 @@
-export interface UserInfoProps {
-    name: string,
-    age: number
-}
-
-
-
-
-
-
 export type StudentProps  = {
     name: string;
     gender: boolean;
     age: number;
+}
+export interface UserInfoProps {
+    name: string,
+    age: number
 }
