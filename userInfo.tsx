c9dde2a86bb1ec6bdf1619f4609a147a1f3e9964
hcc <healthyhcc@gmail.com>
2023-01-07 19:49:55 +0800

    update user info props

--- userInfo.tsx
+++ userInfo.tsx
@@ -2,3 +2,15 @@ export interface UserInfoProps {
     name: string,
     age: number
 }
+export interface UserInfoProps4 {
+    name4: string,
+    age4: number
+}
+export interface UserInfoProps5 {
+    name5: string,
+    age5: number
+}
+export interface UserInfoProps6 {
+    name6: string,
+    age6: number
+}
